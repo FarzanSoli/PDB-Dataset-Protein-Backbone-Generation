342998765a20ab494886c13f18db503c0b4c401e
Farzan Soli <44372399+FarzanSoli@users.noreply.github.com>
2023-11-22 21:47:42 -0500

    Updated

---
+++ Codes/Functions.py
@@ -0,0 +1,160 @@
+""" ########## Processing C-alpha files ########## """
+import math
+import wget
+import copy
+import torch
+import pickle
+import zipfile
+import numpy as np
+import pandas as pd 
+import gzip, shutil
+import os, sys, gemmi, json
+from AA_features import features
+from scipy.spatial import distance
+from sklearn.preprocessing import MinMaxScaler
+# ========================================= #
+class Functions():
+    def __init__(self, directory):
+        super().__init__()
+        self.directory = directory
+    # ========================================= #
+    #                Fix AA sequenc             #
+    # ========================================= #
+    def missing_seq(self, Dataset):
+        df = Dataset.loc[Dataset['Sequence'].str.contains('X') & 
+                           Dataset['Sequence'].str.startswith('X') &
+                           Dataset['Sequence'].str.endswith('X')]
+        for i in ['A','C','D','E','F','G','H','I','K','L',
+                  'M','N','P','Q','R','S','T','V','W','Y']:
+            kn_seq = df.loc[Dataset['Sequence'].str.contains(i)]
+        result = pd.concat([df, kn_seq]).drop_duplicates(keep=False)
+        return result
+    # ========================================= #
+    #                 Read Dataset              #
+    # ========================================= #
+    def Dataset_Reader(self, dataset):
+        Dataset = pd.concat([pd.read_csv('Dataset/'+dataset), 
+                             self.missing_seq(pd.read_csv(
+                            'Dataset/'+dataset))]).drop_duplicates(keep=False)
+        return Dataset
+    # ========================================= #
+    #                   Padding                 #
+    # ========================================= #
+    def padding(self, pad_len, matrix):
+        mat = np.zeros((pad_len, 3))
+        mat[:matrix.shape[0], :matrix.shape[-1]] = matrix
+        return mat
+    # ========================================= #
+    #                  Unzip files              #
+    # ========================================= #
+    def unzip(self, directory, file):
+        # file = 'PDB alpha-C.zip'
+        # directory = "/Dataset/"
+        with zipfile.ZipFile(os.getcwd()+directory+file, 'r') as zip_ref:
+            zip_ref.extractall(os.getcwd()+directory)
+        
+    # ========================================= #
+    #                 Extract gzip              #
+    # ========================================= #
+    def gz_extract(self):
+        extension = ".gz"
+        os.chdir(self.directory)
+        for item in os.listdir(self.directory): # loop through items in dir
+          if item.endswith(extension): # check for ".gz" extension
+              gz_name = os.path.abspath(item) # get full path of files
+              # get file name for file within -> removes '.cif'
+              file_name = (os.path.basename(gz_name)).rsplit('.',1)[0] 
+              with gzip.open(gz_name, "rb") as f_in, open(file_name, "wb") as f_out:
+                  # Copy the contents of source file to destination file
+                  shutil.copyfileobj(f_in, f_out)
+              os.remove(gz_name) # delete zipped file
+    # ========================================= #
+    #               Encoding Amio acids         #
+    # ========================================= #
+    def Normalize_AA(self):
+        props = []
+        for t in range(len(features().AA_prop_keys)):
+            prop = []
+            for i in range(len(list(features().AA_dict))):
+                prop.append(features().Amino_acids[
+                    list(features().AA_dict)[i]][features().AA_prop_keys[t]])
+            props.append(np.array(prop))
+        # ------------------------------------- #
+        Norm_props = []
+        for t in range(len(features().AA_prop_keys)):
+            Norm_props.append(
+                (props[t]-np.min(props[t]))/(np.max(props[t])-np.min(props[t])))
+        # ------------------------------------- #
+        AA_props = []
+        for t in range(len(list(features().AA_dict))):
+            aa_props = []
+            for i in range(len(features().AA_prop_keys)):
+                aa_props.append(Norm_props[i][t])
+            AA_props.append(aa_props)
+        return Norm_props, AA_props
+    # ========================================= #
+    #               Amino acid Encoding         #
+    # ========================================= #
+    def encode_CT(self, Pad_Length, dataframe):
+        encoding = dict(zip(list(features().AA_dict), self.Normalize_AA()[1]))
+        # ------------------------------------- #
+        Encoded_AA = {}
+        for index, row in dataframe.iterrows():
+            Encoded_AA[row['PDB_ID']] = np.array([encoding[c.upper()] for c in row['Sequence']])
+        # ------------------------------------- #
+        encoded_AA = np.zeros((Pad_Length, len(features().AA_prop_keys)))
+        Encoded_AA_padded = {}
+        for key, value in Encoded_AA.items():
+            if value.shape[0] > Pad_Length:
+                encoded_AA[:,:] = value[:Pad_Length,:]
+            else: 
+                encoded_AA[:value.shape[0],:] = value
+            Encoded_AA_padded[key] = encoded_AA
+        return Encoded_AA_padded
+    # ========================================= #
+    #                 READ Fasta file           #
+    # ========================================= #
+    def read_fasta(self, fasta_file, comment="#"):
+        # with gzip.open(fasta_file,"r") as f:
+        with open(fasta_file, "r") as file:
+            id_ = None
+            seq_id = []
+            sequence = []
+            sequences = []
+            # loop through each line in the file
+            for line in file:
+                # If the line starts with a ">" character, it is a new sequence identifier
+                if line.startswith(">"):
+                    # If this is not the first sequence, print the previous one
+                    if id_ is not None:
+                        seq_id.append(id_)
+                        sequences.append(''.join(sequence))
+                    # Get the new sequence identifier and reset the sequence variable
+                    id_ = line.strip()[1:]
+                    sequence = []
+                # Otherwise, it is part of the sequence, so append it to the sequence variable
+                else:
+                    sequence.append(line.upper())
+            if id_ is not None:
+                seq_id.append(id_)
+                sequences.append(''.join(sequence))
+            return list(zip(seq_id, sequences))
+    # ========================================= #
+    #                 Standardize               #
+    # ========================================= #
+    def standardize(self, coordinates):
+        x_standard = (coordinates[:,0] - coordinates[:,0].mean())/(np.std(coordinates[:,0]))
+        y_standard = (coordinates[:,1] - coordinates[:,1].mean())/(np.std(coordinates[:,1]))
+        z_standard = (coordinates[:,2] - coordinates[:,2].mean())/(np.std(coordinates[:,2]))
+        standard_coordinate = np.transpose(np.array([x_standard, y_standard, z_standard]))
+        return standard_coordinate
+    # ========================================= #
+    #                 Normalize               #
+    # ========================================= #
+    def Normalize(self, coordinates):
+        x_norm = (coordinates[:,0] - np.min(coordinates[:,0]))/(np.max(coordinates[:,0]) - np.min(coordinates[:,0]))
+        y_norm = (coordinates[:,1] - np.min(coordinates[:,1]))/(np.max(coordinates[:,1]) - np.min(coordinates[:,1]))
+        z_norm = (coordinates[:,2] - np.min(coordinates[:,2]))/(np.max(coordinates[:,2]) - np.min(coordinates[:,2]))
+        Normalized_coordinate = np.transpose(np.array([x_norm, y_norm, z_norm]))
+
+        return Normalized_coordinate
---
+++ Codes/Protein_Backbone_Dataset.py
@@ -0,0 +1,112 @@
+""" ########## Processing Protein backbone coordinates and distance matrices ########## """
+import os
+import torch
+import pickle
+import numpy as np
+import pandas as pd 
+from Functions import Functions
+from scipy.spatial import distance
+# ========================================= #
+padding = Functions("/Dataset/").padding
+encode_CT = Functions("/Dataset/").encode_CT
+standardize = Functions("/Dataset/").standardize
+# ========================================= #
+class Protein_Backbone():
+    def __init__(self, directory):
+        super().__init__()
+        self.directory = directory
+    # ========================================= #
+    #                 Distance Matrix           #
+    # ========================================= #
+    def Distance_Matrix(self, Pad_Length, Directory, files):
+        Coordinates = {}
+        Protein_backbone = {}
+        Dist_Protein_backbone = {}
+        for file in files: 
+            coordinate = pd.read_excel(Directory +'/'+file, 
+                            names=['X_coordinate', 'Y_coordinate', 'Z_coordinate']).to_numpy()
+                # -------------------------------------------- #
+            if coordinate.shape[0] > Pad_Length:
+                cut_coordinate = coordinate[:Pad_Length, :]
+                # cut_coordinate = self.standardize(cut_coordinate)
+                
+                Coordinates[file.replace('.xlsx','')] = cut_coordinate
+                # -------------------------------------------- #
+                Protein_backbone[file.replace('.xlsx','')] = cut_coordinate
+                Dist_Protein_backbone[file.replace('.xlsx','')] = distance.cdist(
+                                padding(Pad_Length, Protein_backbone[file.replace('.xlsx','')]), 
+                                padding(Pad_Length, Protein_backbone[file.replace('.xlsx','')]), 
+                                'euclidean')
+                # -------------------------------------------- #
+            else:
+                coordinates_ = padding(Pad_Length, coordinate)
+                # coordinates_ = self.standardize(coordinates_)                
+                Coordinates[file.replace('.xlsx','')] = coordinates_
+                # -------------------------------------------- #
+                Protein_backbone[file.replace('.xlsx','')] = Coordinates[file.replace('.xlsx','')][:,:]
+                Dist_Protein_backbone[file.replace('.xlsx','')] = distance.cdist(
+                                padding(Pad_Length, Protein_backbone[file.replace('.xlsx','')]), 
+                                padding(Pad_Length, Protein_backbone[file.replace('.xlsx','')]), 
+                                'euclidean')
+                
+        with open('Dataset/Distance_Protein_Backbone.pkl', 'wb') as file:
+            pickle.dump(Dist_Protein_backbone, file)
+        with open('Dataset/Protein_Backbone.pkl', 'wb') as file:
+            pickle.dump(Protein_backbone, file)
+    # ========================================= #
+    #         Normalize Distance Matrix         #
+    # ========================================= #
+    def Dist_Norm(self, distance_matrix):
+        return (distance_matrix - np.min(distance_matrix))/(np.max(distance_matrix) - np.min(distance_matrix))
+    # ========================================= #
+    #         Encoded Protein Backbone Seq      #
+    # ========================================= #
+    def Encoded_Backbone_Seq(self, Pad_Length, Directory):
+        # Directory = 'Dataset/AA_Seq_main.csv'
+        AA_dataset = pd.read_csv(Directory)
+        Encoded_AA = encode_CT(Pad_Length, AA_dataset)
+        with open('Dataset/Encoded_Backbone_Seq.pkl', 'wb') as file:
+            pickle.dump(Encoded_AA, file)
+    # ========================================= #
+    #           Padding Protein backbone        #
+    # ========================================= #
+    def Padding_Protein_Backbone(self, Pad_Length, Encoded_AA):
+        # Pad_Length = 128
+        Backbone_Seq = {}
+        for key, value in Encoded_AA.items():
+            Backbone_Seq[key] = np.zeros((Pad_Length, value.shape[-1]))
+            Backbone_Seq[key][:value[:Pad_Length].shape[0],
+                           :value[:Pad_Length].shape[-1]] = value[:Pad_Length]
+        # -----------------------------------------------------
+        with open('Dataset/Padded_Backbone_Seq.pkl', 'wb') as file:
+            pickle.dump(Backbone_Seq, file)
+
+# ------------ calculate distance matrix ------------ #
+Pad_Length = 128
+data_dir = os.getcwd() +'/Dataset/PDB_alpha_C'
+files = os.listdir(data_dir)
+Load_Data = Protein_Backbone(data_dir).Distance_Matrix(Pad_Length, data_dir, files)
+
+# ------------ Proteins ------------ #
+with open('Dataset/Distance_Protein_Backbone.pkl', 'rb') as file:
+    Proteins = pickle.load(file)
+    file.close()
+Proteins_PDB_ID = Proteins.keys()
+Proteins_dist = []
+for key, value in Proteins.items():
+    value_scaled = Protein_Backbone(data_dir).Dist_Norm(value)
+    Proteins_dist.append(torch.FloatTensor(value_scaled[None,:,:]))
+
+torch.save(Proteins_dist, 'Dataset/Proteins_Tensor.pt')
+torch.save(list(Proteins.keys()), 'Dataset/Proteins_PDB_ID.pt')
+
+# ------------ Encoded Backbone ------------ #
+Pad_Length = 64
+Directory = 'Dataset/AA_Seq_main.csv'
+Encoded_Backbone_Seq = Protein_Backbone(Directory).Encoded_Backbone_Seq(Pad_Length, Directory)
+
+# ------------ Padding encoded backbone ------------ #
+file = open('Dataset/Encoded_Backbone_Seq_64.pkl', 'rb')
+Dataset = pickle.load(file)
+file.close()
+Padding_Protein_Backbone = Protein_Backbone(Directory).Padding_Protein_Backbone(Pad_Length, Dataset)
